7705611ce72c60b84b7157aa88947da00f2e4b87
Pooja Srivastava <bioinfor.pooja@gmail.com>
2017-11-07 14:57:05 -0500

    nodemon

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const express = require( 'express' );
-const app = express();	
+const express=require('express');
+const app=express();
 
 
-app.get('/', (req, res) => res.send('Hello World'))
+app.use('/',function(req, res, next){
+	console.log("Get / ");
+	next();
+}, function(req, res, next){
+	console.log('Get / is-anybody-in-there')
+	next();
+},function(req, res, next){
+	console.log('Post /modernism')
+	next()
+})
+
+app.use('/special/', function(req, res, next){
+	console.log('Get / 200')
+	next();
+})
+
+app.get('/special/', function(req, res, next){
+	res.send('getting special')
+})
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/news', (req, res) => res.send('hi there!'))
 
 
-app.get('/news', (req, res) => res.send('next msg'))
-app.listen(3000, () => console.log("server listening"))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
